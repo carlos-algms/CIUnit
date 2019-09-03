69ab38f4f53b11eebffe7d77f7dbe7ec6d4b6f0d
Maneesh Kumar <maneesh@chargepoint.com>
2019-09-03 15:36:42 +0530

    Added invokeMethod to call the private/protected functions for writting unit test cases (#12)

--- src/libraries/CIUnitTestCase.php
+++ src/libraries/CIUnitTestCase.php
@@ -313,4 +313,12 @@ abstract class CIUnit_TestCase extends PHPUnit_Framework_TestCase
             }
         }
     }
+    
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
 }
